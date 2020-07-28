cd4adb7498259f5a80394702e950884c6d7955eb
dr-robin <robinmcgregor@gmail.com>
2020-07-28 16:33:48 +0200

    landing page and user address form

--- 3D_house_app.py
+++ 3D_house_app.py
@@ -1,55 +1,60 @@
 from flask import Flask
-from useraddress import get_address
+#from useraddress import get_address
 
 from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def user_address():
 
-	if request.method == ['POST', 'GET']:
-		house_number = request.form.getlist('house_number')
-		street_name = request.form.getlist('street_name')
-		city = request.form.getlist('city')
-		postcode = request.form.getlist('postcode')
-		address = print(x, y, z, q)
+	if request.method == 'POST':
+		house_number = request.form.get('house_number')
+		street_name = request.form.get('street_name')
+		city = request.form.get('city')
+		postcode = request.form.get('postcode')
+		
+		#return success
+		#add 3D house builder function here
+		return '''success'''
+
+	#else return empty form variables
 	else:
 		house_number = ''
 		street_name = ''
 		city = ''
 		postcode = ''
-		address = ''
-	return render_template_string('''
-		<html>
-		<body>
-		<h1>3D house builder</h1>
-		<p> This little app can build a 3D representation of any address in Flanders</p>
-		<h2>Please enter an address in Flanders</h2>
-		<form action='/' method=['POST','GET']>
-		<p>
-		<label for='house_number'>House number</label>
-		<input name='house_number' id='house_number' value="{{house_number}}" />
-		</p>
-		<p>
-		<label for='street_name'>Street name</label>
-		<input name='street_name' id='street_name' value='{{street_name}}' />
-		</p>
-		<p>
-		<label for='city'>City</label>
-		<input name='city' id='city' value='{{city}}'/>
-		</p>
-		<p>
-		<label for='postcode'>Postcode</label>
-		<input name='postcode' id='postcode' value='{{postcode}}'/>
-		</p>
-		<p>
-		<input type='submit' value='Submit' />
-		</p>
-		</form>
-		</body>
-</html>''', house_number=house_number, street_name=street_name, city=city, postcode=postcode, address=address)
 
+	#Landing page with user address form
+	return render_template_string('''
+	<html>
+	<body>
+	<h1>3D house builder</h1>
+	<p> This little app can build a 3D representation of any address in Flanders</p>
+	<h2>Please enter an address in Flanders</h2>
+	<form method='POST'>
+	<p>
+	<label for='house_number'>House number</label>
+	<input name='house_number' id='house_number' value='' />
+	</p>
+	<p>
+	<label for='street_name'>Street name</label>
+	<input name='street_name' id='street_name' value='' />
+	</p>
+	<p>
+	<label for='city'>City</label>
+	<input name='city' id='city' value='' />
+	</p>
+	<p>
+	<label for='postcode'>Postcode</label>
+	<input name='postcode' id='postcode' value='' />
+	</p>
+	<p>
+	<input type='submit' value='Submit' />
+	</p>
+	</form>
+	</body>
+	</html>''')
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
